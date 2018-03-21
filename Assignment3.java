277b8bdf501e56c981cc36f27398beaa1b028bf7
Mohaimen Khan <mohaimen.khan@mail.utoronto.ca>
2018-03-20 21:50:28 -0400

    Everything passes for part 1 of assignment 3

--- Assignment3.java
+++ Assignment3.java
@@ -61,8 +61,43 @@ public class Assignment3 extends JDBCSubmission {
     @Override
     public List<Integer> findSimilarParties(Integer partyId, Float threshold) {
 	//Write your code here.
-        //PreparedStatement execStatement= connection.prepareStatement("");
-        return null;
+        try{
+                // find description of particular party
+            PreparedStatement findPartyDescription = connection.prepareStatement("select description from party where id=?");
+            findPartyDescription.setInt(1, partyId);
+            ResultSet descriptionResults = findPartyDescription.executeQuery();
+            List <String> givenDescriptions = new ArrayList<String> ();
+            while (descriptionResults.next()){
+                String addThis = descriptionResults.getString(1);
+                if (addThis != null)
+                    givenDescriptions.add(addThis); // if a party has more than one description
+            }
+
+                // find all other descriptions
+            PreparedStatement execStatement = connection.prepareStatement("select description, id from party");
+            ResultSet execResults = execStatement.executeQuery();
+            List<Integer> finalAns = new ArrayList<Integer> ();
+
+            // parse all the results
+            while (execResults.next()){
+                String description = execResults.getString(1); // this is the description of the individual string
+                int party_id = execResults.getInt(2); // get the partyID
+                if (party_id != partyId){    
+                    // match it against other descriptions
+                    for (int i=0; i < givenDescriptions.size(); i++){
+                        String given = givenDescriptions.get(i);
+                        double similarity_ = similarity(given, description);
+                        if (similarity_ > threshold)
+                            finalAns.add(party_id);                    
+                    }
+                }
+            }
+            return finalAns;
+        }
+        catch (SQLException ex){
+            return null;
+        }
+        
     }
 
     public static void main(String[] args) throws Exception {
